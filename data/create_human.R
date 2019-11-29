bc62fb985424f9bd9306c69356b9575d4aa99c4d
tnissinen <tnissinen10@gmail.com>
2019-11-29 10:09:37 +0200

    Implemented exercise 5 data wrangling part.

--- data/create_human.R
+++ data/create_human.R
@@ -1,5 +1,10 @@
 # Tomi Nissinen, 22.11.2019, data wrangling exercise
 # Dataset from http://hdr.undp.org/en/content/human-development-index-hdi
+
+#############
+# Exercise 4:
+#############
+
 library(dplyr)
 
 
@@ -71,5 +76,66 @@ glimpse(human)
 setwd("C:/Users/tomni/Documents/IODS-project")
 
 # write to csv file
-write.csv(alc, "data/human.csv", row.names = FALSE)
+write.csv(human, "data/human.csv", row.names = FALSE)
+
+
+#############
+# Exercise 5:
+#############
+
+library(stringr)
+
+# my previous data wrangling exercise was not perfect so I load the downloaded human1.txt file
+human <- read.table("data/human1.txt", sep  =",", header = T)
+
+# look at the (column) names of human
+names(human)
+
+  # look at the structure of human
+  str(human)
+
+# print out summaries of the variables
+summary(human)
+
+# check last few rows
+tail(human)
+
+# mutate GNI to numeric
+human <- mutate(human, GNI = as.numeric(str_replace(GNI, pattern=",", replace ="")))
+
+# check the tail again with some more rows
+tail(human, 20)
+
+# columns to keep
+keep <- c("Country", "Edu2.FM", "Labo.FM", "Life.Exp", "Edu.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+
+# select the 'keep' columns
+human <- select(human, one_of(keep))
+
+# print out a completeness indicator of the 'human' data
+complete.cases(human)
+
+# print out the data along with a completeness indicator as the last column
+data.frame(human[-1], comp = complete.cases(human))
+
+# filter out all rows with NA values
+human <- filter(human, complete.cases(human))
+
+# select only the country rows and not regions
+human <- human[1:155, ]
+
+# set Country as row name
+rownames(human) <- human$Country
+
+# remove the Country variable
+human <- select(human, -Country)
+
+# check structure again
+str(human)
+
+# check tail again
+tail(human, 60)
+
+# write to csv file
+write.csv(human, "data/human.csv")
 
